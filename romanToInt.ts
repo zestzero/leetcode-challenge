059a2e23920814b42a6dbacdbf51804e809814e4
Krit B <gmkipladen@gmail.com>
2023-08-12 13:53:53 +0700

    add romanToInt

---
+++ romanToInt.ts
@@ -0,0 +1,21 @@
+const values = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+};
+
+function romanToInt(s: string): number {
+    let sum = 0;
+    for(let counter = 0; counter < s.length; counter++) {
+        if (counter > 0 && values[s[counter - 1]] < values[s[counter]]) {
+            sum += values[s[counter]] - (values[s[counter - 1]] * 2);
+        } else {
+            sum += values[s[counter]];
+        }
+    }
+    return sum;
+};
